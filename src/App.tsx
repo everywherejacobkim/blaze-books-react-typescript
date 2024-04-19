198d4e402e7463c65348d0f5f48790cd45fd61b5
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2024-04-19 13:51:04 -0700

    Build: create jumbotro component & styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import Header from "./components/Header";
+import Header from "./components/header/Header";
+import Jumbotron from "./components/jumbotron/Jumbotron";
 
 function App() {
   return (
     <>
-      <Header title="Blaze Books"/>
+      <Header title="Blaze Books" />
+      <Jumbotron
+        title="Let Blaze Books light up your reading journey!"
+        description="Explore, Discover, Read!"
+        btnText="Add Book"
+      />
     </>
   );
 }
---
+++ src/components/jumbotron/Jumbotron.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import bookImage from "../../assets/images/books-jumbo.jpg";
+
+interface JumbotronProps {
+  title: string;
+  description: string;
+  btnText: string;
+}
+
+const Jumbotron: React.FC<JumbotronProps> = ({
+  title,
+  description,
+  btnText,
+}) => {
+  return (
+    <div className={styles["jumbotron-container"]}>
+      <div className={styles["content"]}>
+        <div className={styles["image-container"]}>
+          <img src={bookImage} alt={title} />
+        </div>
+        <div className={styles["text"]}>
+          <h1 className={styles["title"]}>{title}</h1>
+          <p className={styles["description"]}>{description}</p>
+          <div className={styles["button-container"]}>
+            <button className={styles["button"]}>{btnText || "Button"}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
